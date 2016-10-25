589ce2e95ab05ba09405199b72a910e71a556cae
Mark Torok <torok.marko@gmail.com>
2016-10-25 22:40:28 +0200

    Add more files to sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -2,7 +2,7 @@
 ############################
 # .make.sh
 # This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# based on: https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
+# based on/ https://github.com/michaeljsmalley/dotfiles/blob/master/makesymlinks.sh
 ############################
 
 ########## Variables
@@ -10,7 +10,14 @@
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 #files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
-files="shell:aliases shell:bash_prompt shell:bashrc tool:curlrc tool:wgetrc ruby:gemrc ruby:rspec git:gitattributes git:gitignore git:gitconfig git:gitmessage vim:vimrc vim:vim"
+files="
+shell/bash_aliases shell/bash_autocomplete shell/bash_colors shell/bash_exports
+shell/bash_functions shell/bash_options shell/bash_profile shell/bash_prompt
+shell/bashrc
+tool/curlrc tool/wgetrc
+ruby/gemrc ruby/rspec
+git/gitattributes git/gitignore git/gitconfig git/gitmessage vim/vimrc
+"
 ##########
 
 # create dotfiles_old in homedir
@@ -25,14 +32,13 @@ echo "done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
-	idx=`expr index $file :`
+	idx=`expr index $file /`
 	purefile=${file:idx}
 	if [ -e "../.$purefile" ]; then
     	echo "Moving any existing dotfiles from ~ to $olddir"
     	mv ~/.$purefile ~/dotfiles_old/
 	fi
     echo "Creating symlink to $purefile in home directory."
-	link=${file//":"/"/"}
     ln -s $dir/$link ~/.$purefile
 done
 
